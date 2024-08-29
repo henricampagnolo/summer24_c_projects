8fb37bac99195b30c62f51dfa35757a82af44572
henricampagnolo <121348195+henricampagnolo@users.noreply.github.com>
2024-08-29 01:11:14 +0100

    Add files via upload

---
+++ c08/ex01/ft_boolean.h
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+#define TRUE 1
+#define FALSE 0
+
+#define EVEN(X) ((X % 2) == 0)
+
+#define EVEN_MSG "I have an even number of arguments."
+
+#define ODD_MSG "I have an odd number of arguments."
+
+#define SUCCESS 0
+
+typedef short int t_bool;
---
+++ c08/ex02/main.c
@@ -0,0 +1,12 @@
+#include "ft_abs.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	int b;
+
+	a = -456;
+	b = 34;
+	printf("Abs values, of a: %d , and of b: %d \n", ABS(a), ABS(b));
+}
